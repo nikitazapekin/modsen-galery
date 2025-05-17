aebf178ead957ffdc107f686c0ba980f4913b5d7
nikitazapekin <wotblitz362@mail.ru>
2025-05-17 15:44:47 +0300

    feat(useLocalStorage): hook for work with local storage was crated

--- src/components/ImagesListItem/ImagesListItem.tsx
+++ src/components/ImagesListItem/ImagesListItem.tsx
@@ -1,4 +1,45 @@
-import { useState } from "react"
+import {
+  ImageItem,
+  ImageItemIcon,
+  ImageItemIconWrapper,
+  ImageItemPhoto,
+  ImageItemPreview,
+  ImageItemText,
+} from "./ImagesListItem.style"
+import { ImagesListItemProps } from "./ImagesListItem.types"
+import Notion from "@assets/icons/NotionActive.svg"
+import NotionDisabled from "@assets/icons/NotionDisabled.svg"
+import useLocalStorageCards from "@/hooks/useLocalStorage"
+
+const ImagesListItem = ({ card }: ImagesListItemProps) => {
+  const truncateText = (text: string, maxLength: number) => {
+    if (!text) return "Title is missed"
+    return text.length > maxLength ? `${text.substring(0, maxLength)}...` : text
+  }
+
+  const { isCardSaved, toggleCard } = useLocalStorageCards()
+
+  const handleStore = () => {
+    toggleCard(card)
+  }
+
+  return (
+    <ImageItem>
+      <ImageItemPhoto src={card.urls.full} />
+
+      <ImageItemPreview>
+        <ImageItemText>{truncateText(card.description, 20)}</ImageItemText>
+        <ImageItemIconWrapper onClick={handleStore}>
+          <ImageItemIcon src={isCardSaved(card.id) ? Notion : NotionDisabled} alt={"Icon"} />
+        </ImageItemIconWrapper>
+      </ImageItemPreview>
+    </ImageItem>
+  )
+}
+
+export default ImagesListItem
+
+/* import { useState } from "react"
 import {
   ImageItem,
   ImageItemIcon,
@@ -34,3 +75,4 @@ const ImagesListItem = ({ card }: ImagesListItemProps) => {
 }
 
 export default ImagesListItem
+ */
