89a41589eccf8805d0126d6ed314fcc79a7564d2
nikitazapekin <wotblitz362@mail.ru>
2025-05-20 01:42:13 +0300

    fix: fix dependency issues

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@assets": path.resolve(__dirname, "./src/assets"),
